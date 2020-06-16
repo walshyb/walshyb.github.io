ffbfc6f9ea512dfc5f89180fe01ac3451dfc64c5
Brandon <bmwalshy@gmail.com>
2020-06-16 12:45:35 -0400

    Add react router
    Even though it's not needed...

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from './components/shell/Header';
 import Footer from './components/shell/Footer';
+import { Route, Switch } from 'react-router-dom';
+import Intro from './components/page-components/Intro';
+import About from './components/page-components/About';
+import Projects from './components/page-components/Projects';
 
 function App() {
   return (
     <div id="root">
       <Header />
+      <div className="content">
+        <Switch>
+          <Route path="/">
+            <Intro />
+            <About />
+            <Projects />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
     </div>
   );
