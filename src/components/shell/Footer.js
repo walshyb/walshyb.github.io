c3b2e9f226cb31bce7db0d8997f8303d783eb183
Brandon <bmwalshy@gmail.com>
2020-06-16 12:18:00 -0400

    Add Footer and Header shells

---
+++ src/components/shell/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Footer.scss';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+      </footer>
+    );
+  }
+}
+
