d983789cd2e24c857bda18f6a1bbe8fcdc31b0ce
Brandon <bmwalshy@gmail.com>
2020-06-18 15:59:07 -0400

    Add shell for project links

--- src/components/common/ProjectItem.js
+++ src/components/common/ProjectItem.js
@@ -7,23 +7,29 @@ export default class ProjectItem extends Component {
     name: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     tags: PropTypes.arrayOf(PropTypes.string).isRequired,
-    content: PropTypes.string.isRequired
+    content: PropTypes.string.isRequired,
+    caseStudyUrl: PropTypes.string,
+    liveSiteUrl: PropTypes.string
   };
 
   render() {
     const { name, image, tags, content } = this.props;
     return (
       <article className="project-item">
-        <img src={ image } alt="" />
-        <h2>{ name }</h2>
-        <div className="tags">
-          {
-            tags.map( tag => {
-              return <Tag name={ tag }/>
-            })
-          }
+        <div className="project-details">
+          <img src={ image } alt="" />
+          <h2>{ name }</h2>
+          <div className="tags">
+            {
+              tags.map( tag => {
+                return <Tag name={ tag }/>
+              })
+            }
+          </div>
+          <p>{ content }</p>
         </div>
-        <p>{ content }</p>
+        <div className="project-links">
+        </div>
       </article>
     );
   }
