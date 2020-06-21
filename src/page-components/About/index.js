1e3f2973cbbf581d02c90ae4e38fbce3a5b31267
Brandon <bmwalshy@gmail.com>
2020-06-21 12:43:02 -0400

    Write the About

--- src/page-components/About/index.js
+++ src/page-components/About/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './index.scss';
+import Tag from '../../components/common/Tag';
 
 export default function About() {
   return (
     <section className="about">
-      <p>Hi, my name is Brandon. I'm a web developer currently based in the Hudson Valley, NY with plans to move back to my home in NYC. Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere.</p>
+      <p>Hi, my name is Brandon. I'm a web developer currently based in the Hudson Valley, NY with plans to move back to my home in NYC. I have 5+ years of experience across the stack and I'm particularly interested in backend technologies.</p>
+      <p>The technologies I've used most in the last few years are <Tag name="rails"/>, <Tag name="react"/>, and <Tag name="wordpress"/>. My experience includes: documenting &amp; creating APIs, building custom Content Management Systems, writing tests, building react components &amp; sites, and planning &amp; managing database systems. </p>
     </section>
   );
 }
