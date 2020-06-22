893bfa8b348131537010d60faac5bd38bb8125f9
Brandon <bmwalshy@gmail.com>
2020-06-22 02:21:12 -0400

    Add images to project details

---
+++ src/lib/project-data.js
@@ -0,0 +1,32 @@
+import NewPaltzLibraryImage from '../images/new-paltz-library.png';
+import RadioKingstonImage from '../images/radio-kingston.png';
+import CatskillsConfImage from '../images/catskills-conf.png';
+
+const projectData = [
+  {
+      name: 'SUNY New Paltz Library',
+      image: NewPaltzLibraryImage,
+      tags: ['wordpress', 'jquery', 'php', 'sass'],
+      content: 'Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere.',
+      liveSiteUrl: 'https://google.com',
+      caseStudyUrl: 'https://google.com'
+  },
+  {
+      name: 'Radio Kingston',
+      image: RadioKingstonImage,
+      tags: ['react', 'es6', 'sass', 'node', 'wordpress'],
+      content: 'Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere.',
+      liveSiteUrl: 'https://google.com',
+      caseStudyUrl: 'https://google.com'
+  },
+  {
+      name: 'Catskills Conf',
+      image: CatskillsConfImage,
+      tags: ['ruby', 'rails', 'react'],
+      content: 'Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere.',
+      liveSiteUrl: 'https://google.com',
+      caseStudyUrl: 'https://google.com'
+  },
+];
+
+export default projectData;
