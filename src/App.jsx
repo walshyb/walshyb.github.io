470e13f4ce5ee08974060a8723e90f2f4e25566f
Brandon Walsh <bmwalshy@gmail.com>
2024-08-05 20:36:27 -0400

    Add basic intro box for social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/Header";
+import IntroBox from "./components/IntroBox";
 
 function App() {
   return (
     <div className="app-container">
       <Header />
+      <IntroBox />
     </div>
   );
 }
---
+++ src/components/IntroBox.jsx
@@ -0,0 +1,24 @@
+import "./IntroBox.scss";
+
+export default function IntroBox() {
+  return (
+    <section className="intro-box">
+      <div className="intro-img-container">
+        <img className="intro-img" src="https://placehold.co/300x300" />
+      </div>
+      <div className="intro-content">
+        <div className="intro-text">
+          <p>Howdy!</p>
+          <p>Some stuff</p>
+          <p>Or jump down to other stuff</p>
+        </div>
+
+        <div className="affiliate-banners">
+          <img className="affiliate-banner" src="https://placehold.co/81x33" />
+          <img className="affiliate-banner" src="https://placehold.co/81x33" />
+          <img className="affiliate-banner" src="https://placehold.co/81x33" />
+        </div>
+      </div>
+    </section>
+  );
+}
