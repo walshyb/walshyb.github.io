3801da9bbd61b7ba7a69a3d431403452eae68452
walshyb <bmwalshy@gmail.com>
2024-08-16 00:34:26 -0400

    Basic font scaling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,12 @@ export default function Projects() {
   return (
     <section className="projects-container">
       <h2>Projects</h2>
-      <div className="projects">ahhh</div>
+      <div className="projects">
+        <div className="project">
+          <img className="intro-img" src="https://placehold.co/200x200" />
+          <div className="project-details">Title</div>
+        </div>
+      </div>
     </section>
   );
 }
