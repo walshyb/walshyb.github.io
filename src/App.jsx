4b128993c229083a47a1e01dd6663882dc480443
walshyb <bmwalshy@gmail.com>
2024-12-04 13:10:00 -0500

    Jump to anchor on load if present

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,21 @@ import IntroBox from "./components/IntroBox";
 import Projects from "./components/Projects";
 import Modal from "./components/Modal";
 import About from "./components/About";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [modalId, openModal] = useState("");
+
+  // Handle case of hash in url on load
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const element = document.getElementById(hash.substring(1));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, []);
 
   return (
     <div className="app-container">
